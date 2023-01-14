a2fd7e77cf69c5019e51a4a91dfd9ddedb95357d
hassank185 <hk185429@gmail.comgit config --global user.email hk185429@gmail.comgit config --global user.email hk185429@gmail.com>
2023-01-14 21:38:23 +0500

    commit 'dart datatypes'

---
+++ dart_course/bin/dart_course.dart
@@ -0,0 +1,30 @@
+void main(){
+  //Numbers , Strings , Boolean , Lists , Maps , dynamic
+  
+  
+  //Strings
+  String myName = 'john';
+  print("String: $myName");
+  
+  //Integers
+  int myNumber = 95;
+    print("Integer: $myNumber");
+
+
+  
+  //double
+  double percentage = 76.7;
+    print("Double: $percentage");
+
+ 
+  
+  //Boolean
+  bool isTrue = false;
+  print("Boolean: $isTrue");
+  
+  
+  //dynamic
+ dynamic myData = 'important files';
+   print("Dynamic: $myData");
+  
+}
